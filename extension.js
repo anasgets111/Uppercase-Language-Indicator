ea56ab17e94246088b570f72e8b5e2a7d4a37d6b
Anas Khalifa <anasgets111@gmail.com>
2025-04-24 00:07:17 +0200

    Performance Optimization by fixing unnecessary function calls in loop, and add a check if indicator isn't available

--- extension.js
+++ extension.js
@@ -1,69 +1,81 @@
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import GObject from 'gi://GObject';
-import {
-    Extension
-} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import GObject from "gi://GObject";
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
-var UppercaseLangIndicator = GObject.registerClass({
-    GTypeName: 'UppercaseLangIndicator',
-}, class UppercaseLangIndicator extends GObject.Object {
-    constructor() {
-        super();
-        this._keyboard = Main.panel.statusArea.keyboard;
-        this._signalId = 0;
-        this._originalIndicatorTexts = [];
-    }
+var UppercaseLangIndicator = GObject.registerClass(
+	{
+		GTypeName: "UppercaseLangIndicator",
+	},
+	class UppercaseLangIndicator extends GObject.Object {
+		constructor() {
+			super();
+			this._keyboard = Main.panel.statusArea.keyboard;
+			this._signalId = 0;
+			this._originalIndicatorTexts = [];
+		}
 
-    enable() {
-        this._signalId = this._keyboard._inputSourceManager.connect('current-source-changed', this._updateIndicator.bind(this));
-        this._updateIndicator();
-    }
+		enable() {
+			// Simple check for language indicator
+			if (!this._keyboard) {
+				console.log("Language indicator not available, extension disabled");
+				return;
+			}
 
-    disable() {
-        if (this._signalId) {
-            this._keyboard._inputSourceManager.disconnect(this._signalId);
-            this._signalId = 0;
-            this._restoreIndicator();
-        }
-    }
+			this._signalId = this._keyboard._inputSourceManager.connect(
+				"current-source-changed",
+				this._updateIndicator.bind(this)
+			);
+			this._updateIndicator();
+		}
 
-    _updateIndicator() {
-        let source = this._keyboard._inputSourceManager.currentSource;
+		disable() {
+			if (this._signalId) {
+				this._keyboard._inputSourceManager.disconnect(this._signalId);
+				this._signalId = 0;
+				this._restoreIndicator();
+			}
+		}
 
-        if (source) {
-            let shortName = source.shortName.toUpperCase();
-            let children = this._keyboard._container.get_children();
-            for (let i = 0; i < children.length; i++) {
-                let child = this._keyboard._container.get_children()[i];
-                if (this._originalIndicatorTexts[i] === undefined) {
-                    this._originalIndicatorTexts[i] = child.get_text();
-                }
-                child.set_text(shortName);
-            }
-        }
-    }
+		_updateIndicator() {
+			let source = this._keyboard._inputSourceManager.currentSource;
 
-    _restoreIndicator() {
-        let children = this._keyboard._container.get_children();
-        for (let i = 0; i < children.length; i++) {
-            if (this._originalIndicatorTexts[i] !== undefined) {
-                this._keyboard._container.get_children()[i].set_text(this._originalIndicatorTexts[i]);
-            }
-        }
-        this._originalIndicatorTexts = [];
-    }
-});
+			if (source) {
+				let shortName = source.shortName.toUpperCase();
+				let children = this._keyboard._container.get_children();
+				for (let i = 0; i < children.length; i++) {
+					let child = children[i];
+					if (this._originalIndicatorTexts[i] === undefined) {
+						this._originalIndicatorTexts[i] = child.get_text();
+					}
+					child.set_text(shortName);
+				}
+			}
+		}
+
+		_restoreIndicator() {
+			let children = this._keyboard._container.get_children();
+			for (let i = 0; i < children.length; i++) {
+				if (this._originalIndicatorTexts[i] !== undefined) {
+					children[i].set_text(this._originalIndicatorTexts[i]);
+				}
+			}
+			this._originalIndicatorTexts = [];
+		}
+	}
+);
 
 let _indicator;
 
 export default class UppercaseLangIndicatorExtension extends Extension {
-    enable() {
-        _indicator = new UppercaseLangIndicator();
-        _indicator.enable();
-    }
+	enable() {
+		_indicator = new UppercaseLangIndicator();
+		_indicator.enable();
+	}
 
-    disable() {
-        _indicator.disable();
-        _indicator = null;
-    }
+	disable() {
+		if (_indicator) {
+			_indicator.disable();
+			_indicator = null;
+		}
+	}
 }
